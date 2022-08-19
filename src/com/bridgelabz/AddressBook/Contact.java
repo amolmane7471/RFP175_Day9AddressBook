bb958cf5d685d1472be4167e3a00d518d9f14d53
amolmane7471 <maneamol3233@gmail.com>
2022-08-19 13:18:07 +0530

    [Amol]: added address book system UC1 for create new contact

---
+++ src/com/bridgelabz/AddressBook/Contact.java
@@ -0,0 +1,76 @@
+package com.bridgelabz.AddressBook;
+
+public class Contact {
+	
+	private String firstName; 
+	private String lastName; 
+	private String address; 
+	private String city ;
+	private String state ;
+	private String email ;
+	private String phoneNo; 
+	private String zip ;
+
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	@Override
+	public String toString() {
+		return "*******************************"+
+				"firstName=" + firstName + "\n"
+				+ ", lastName=" + lastName + "\n"
+				+ ", address=" + address + "\n"
+				+ ", city=" + city + "\n"
+				+ ", state=" + state + "\n"
+				+ ", email=" + email + "\n"
+				+ ", phoneNo=" + phoneNo + "\n" 
+				+ ", zip=" + zip +"\n";
+	}
+
+	
+}
