a395ccc5187a764ae15c7ea492b87c09a29be306
amolmane7471 <maneamol3233@gmail.com>
2022-08-19 12:57:09 +0530

    [Amol]: added address book system program

---
+++ src/com/bridgelabz/AddressBook/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.AddressBook;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To AddressBook System \n");
+
+	}
+
+}
